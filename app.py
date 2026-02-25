dd06ad918a9c9a11600cae283ea73a77f5cb2892
Tarundeep Singh <tarundeepsinghghotra1@gmail.com>
2026-02-25 22:30:45 +0530

    Ignore trained model file

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ if model is None:
 # -------------------------------------------------
 st.markdown("""
 <div class="navbar">
-    <div class="logo">Lumina<span>Predict</span></div>
+    <div class="logo">Intellex<span>Predict</span></div>
     <div class="nav-btn">Access System</div>
 </div>
 """, unsafe_allow_html=True)
